42df0f71d409f9520db9fe04bb58cfd099a7ec59
salman-rafii <salmanrafi125@gmail.com>
2023-02-20 00:26:52 +0500

    working on ad mob integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:pdf_viewer/screens/splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
--- lib/screens/view_pdf.dart
+++ lib/screens/view_pdf.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:pdf_viewer/utils/colors.dart';
 import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
 
-class PdfApp extends StatelessWidget {
+class PdfApp extends StatefulWidget {
   final String path;
   const PdfApp({Key? key, required this.path}) : super(key: key);
+
+  @override
+  State<PdfApp> createState() => _PdfAppState();
+}
+
+class _PdfAppState extends State<PdfApp> {
+  late BannerAd _bannerAd;
+  bool isAdLoaded = false;
+
+  @override
+  void initState() {
+    _initBannerAd();
+    super.initState();
+  }
+
+  _initBannerAd() {
+    _bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: 'BannerAd',
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          isAdLoaded = true;
+        },
+        onAdFailedToLoad: (ad, error) {},
+      ),
+      request: AdRequest(),
+    );
+    _bannerAd.load();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +51,13 @@ class PdfApp extends StatelessWidget {
           "PDF VIEWER",
         ),
       ),
-      body: PdfView(gestureNavigationEnabled: true, path: path),
+      body: PdfView(gestureNavigationEnabled: true, path: widget.path),
+      bottomNavigationBar: isAdLoaded
+          ? Container(
+              height: _bannerAd.size.height.toDouble(),
+              width: _bannerAd.size.width.toDouble(),
+            )
+          : SizedBox(),
     );
   }
 }
